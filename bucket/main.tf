4fa8b96fc572f28362bba57a07597df3f2cbdc4b
otamori <iromato@gmail.com>
2022-03-19 15:16:04 -0300

    aula de terraform

---
+++ bucket/main.tf
@@ -0,0 +1,22 @@
+terraform {
+    required_version = ">= 1.0.0"
+
+    required_providers {
+      aws ={
+          version = "3.73.0"
+          source = "hashicorp/aws"
+      }  
+    }
+    
+}
+
+provider "aws"{
+    region = "eu-central-1"
+
+    default_tags {
+        tags = {
+          owner = "renato"
+          managed-by = "terraform"  
+        }
+    }
+}
